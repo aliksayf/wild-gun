7b6965837f57d9525eaa870acd56552701a0e5c0
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-10-28 21:24:42 -0700

    first commit

---
+++ src/svg/Vector1.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import frame from "./Frame 1.png"
+
+function Vector1 () {
+    return (
+
+        <svg width="140" height="142" viewBox="0 0 140 142" fill="none" >
+            <rect width="140" height="142" fill="url(#pattern0)"/>
+            <defs>
+                <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+                    <use href="Frame 1.png" transform="translate(-0.00714286) scale(0.00253571)"/>
+                </pattern>
+                <image id="image0" width="400" height="400" href="#pattern0"/>
+            </defs>
+        </svg>
+
+    )
+};
+
+export default Vector1;
+
