6862f852898ddde037b521a0941615e1e7d89986
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-11-03 21:07:07 -0800

    round loop

--- src/App.js
+++ src/App.js
@@ -1,50 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import sound from './Bomb.mp3';
+import React, {useEffect, useState} from 'react';
+import Game from './Game';
+import sound from './Gunshot.mp3';
 import './App.css';
-import Figure from "./Figure";
-import x from "./svg/Union.svg"
-import Vector1 from "./svg/Vector1";
-
-const listOfFigures = [{
-    id: 1,
-    order: 'first',
-    bgc: 'darkgrey'
-  },
-  {
-    id: 2,
-    order: 'second',
-    bgc: 'grey'
-  },
-  {
-    id: 3,
-    order: 'third',
-    bgc: 'darkgrey'
-  }]
 
 function App() {
+    const [audio] = useState(new Audio(sound));
 
-  const [audio] = useState(new Audio(sound));
+    const panel = <div></div>
 
-  const toggle = () => {
-      audio.pause();
-      audio.currentTime = 0;
-      audio.play();
-  }
+    let i = 1;
+    const rCycle = () => {
+        setTimeout(() => {
+            i % 2 !== 0 ? setRound(<Game/>) : setRound(panel);
+            i++
+            i < 7 && rCycle();
+        }, 3000)
+    }
 
-  return (
-    <div className={`
+    useEffect(() => {
+        rCycle();
+    }, [])
+
+    const [round, setRound] = useState(panel)
+
+    const toggle = () => {
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    return (
+        <div className={`
       game-zone
       aim
       `}
-         onClick={toggle}
+             onPointerDown={toggle}
         >
-
-      {listOfFigures.map(el =>
-        <Figure key={el.id} el={el} />
-      )}
-      <Vector1/>
-    </div>
-  );
+            {round}
+            {/*<Game/>*/}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Game.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Figure from "./Figure";
+import Vector1 from "./svg/Vector1";
+
+const listOfFigures = [{
+    id: 1,
+    order: 'first',
+    bgc: 'darkgrey'
+  },
+  {
+    id: 2,
+    order: 'second',
+    bgc: 'grey'
+  },
+  {
+    id: 3,
+    order: 'third',
+    bgc: 'darkgrey'
+  }]
+
+function Game() {
+
+
+  return (
+    <>
+      {listOfFigures.map(el =>
+        <Figure key={el.id} el={el} />
+      )}
+      <Vector1/>
+    </>
+  );
+}
+
+export default Game;
